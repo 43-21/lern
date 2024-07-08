f8e83fe7992767ff7e47111e987cea17d6a20877
43-21 <68917362+43-21@users.noreply.github.com>
2024-07-08 13:37:51 +0200

    fix regex

--- src/dictionary/lemmatize.rs
+++ src/dictionary/lemmatize.rs
@@ -4,7 +4,7 @@ use tokio_rusqlite::Result;
 use crate::database::dictionary;
 
 pub async fn lemmatize(text: String) -> Result<Vec<String>> {
-    let regex = Regex::new(r"[^а-ЯёЁ]").unwrap();
+    let regex = Regex::new(r"[^А-яёЁ]").unwrap();
     let words: Vec<String> = regex.replace_all(&text, " ")
         .split_whitespace()
         .map(|s| s.to_lowercase())
