78a9b86cc167d9f8bfc26c7c8bc2b5f16c4dc839
43-21 <68917362+43-21@users.noreply.github.com>
2024-07-15 10:56:03 +0200

    remove unnecessary code

--- src/main.rs
+++ src/main.rs
@@ -6,15 +6,5 @@ mod dictionary;
 mod gui;
 
 fn main() {
-    // tokio::runtime::Builder::new_multi_thread()
-    // .enable_all()
-    // .build()
-    // .unwrap()
-    // .block_on(
-    //     async {
-    //         database::create_dictionary().await.unwrap();
-    //         database::create_schedule().await.unwrap();
-    //     }
-    // );
     gui::run().unwrap();
 }
